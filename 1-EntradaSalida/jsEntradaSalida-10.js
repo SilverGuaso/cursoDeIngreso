52db4fd91df5d39af8d34a10c56b5b1c708cd87c
silverguaso <silverguaso@hotmail.com>
2016-07-02 20:43:23 -0300

    Ejercicios 8 9 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var numero1;
+	var descuento;
+
+	numero1=document.getElementById('importe').value;
+	descuento=parseInt(numero1)*0.75;
+
+	document.getElementById('resultado').value=descuento;
 	
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo1;
+	var resultado1;
+
+	sueldo1=document.getElementById('sueldo').value;
+	resultado1=sueldo1*1.1;
+
+	document.getElementById('resultado').value="Su nuevo salario es de $"+resultado1;
+	
+
 
 	
 }
