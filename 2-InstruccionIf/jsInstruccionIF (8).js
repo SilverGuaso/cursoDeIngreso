4051ea8e44291fd5817e87fcdb72587e97a1fbf3
silverguaso <silverguaso@hotmail.com>
2016-07-03 23:06:07 -0300

    ejercicio 8 IF

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,27 @@
 function Mostrar()
 {
-//tomo la edad  
+	var edad;
+	var estado;
+	edad=document.getElementById('edad').value;
+	estado=document.getElementById('estadoCivil').value;
 
+	if (edad<=18) 
+		{
+
+		} 
+		else if (edad>=18 & estado=='Soltero')
+		 {
+		 	alert("Usted es mayor y es soltero");
+
+		 } else if (edad>=18 & estado=='Casado')
+		  {
+		  	alert("Usted es mayor y es casado");
+
+		  } else if (edad>=18 & estado=='Divorciado')
+		   {
+		   	alert("Usted es mayor y es divorciado");
+
+		   }
 	
 
 
