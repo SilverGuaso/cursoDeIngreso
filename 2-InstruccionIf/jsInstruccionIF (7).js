dc257ee1c42abd1bace91f0000b8b09b2fcbe0d7
silverguaso <silverguaso@hotmail.com>
2016-07-03 22:04:55 -0300

    ejercicio 7 if

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
-//tomo la edad  
+	var edad;
+	var estado;
+	edad=document.getElementById('edad').value;
+	estado=document.getElementById('estadoCivil').value;
+
+	if (edad<=17 & estado!='Soltero')
+	 {
+	 	alert("Es muy pequeño para NO ser soltero");
+	 	
+
+	}
+
+
+
 
 	
 
