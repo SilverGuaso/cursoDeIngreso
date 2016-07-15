a49d60661476188fbcf5faf83ec99e100466b66a
silverguaso <silverguaso@hotmail.com>
2016-07-15 20:42:13 -0300

    ejercicios while clase.

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,11 +1,110 @@
-function Mostrar()
+﻿function Mostrar()
 {
-	i=0;
-	while (i < 10) {
-    document.write(i+= "The number is " + i);
-    i++;
+/*	alert('iteración while');
+var contador;
+contador=0;
+if (contador<10)
+ {
+ 	alert("mensaje "+contador);
+	contador=contador+1//variable= variable + valor literal
+
+ }
+
+if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ }
+ if (contador<10)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+
+ } NO HACER ITERACIONES CON IF*/
+
+var contador;
+contador=0
+/* while (contador<3)
+ {
+ 	alert("mesaje "+contador);
+ 	contador=contador+1//variable= variable + valor literal
+ }
+*/
+/*	while(true)
+	{
+		alert("mensaje "+contador);
+		contador=contador+1;
+		if (contador==3)
+		 {
+		 	break;
+		 }
+		
+	}
+*/
+while (true)
+{
+	contador=contador+1;
+	if (contador==3)
+	 {
+	 	continue;//cuando el contador llega a tres vuelve a empezar el while. en el break SALE del while.
+	 }
+	 alert("while"+contador);
+	 if (contador==6)
+	  {
+	  	break;
+	  }
+	  alert("fin"+contador);
 }
-
-
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador=10;
+	while (contador>0)
+	{
+		alert("contador "+contador);
+		contador--;
+	}
 
+/*	contador=11;
+	while (contador>1)
+	{
+		contador=contador-1;
+		alert("contador "+contador);
+	}
+*/
 
 }//FIN DE LA FUNCIÓN
