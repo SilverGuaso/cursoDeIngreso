c440299051897a9191801523d95eaef374f6d7b7
silverguaso <silverguaso@hotmail.com>
2016-07-20 16:53:50 -0300

    Ejercicio TP Nº12

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -4,16 +4,16 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var numero;
-	while(contador<10)
+	while(contador<5)
 	{
 		contador++;
-		numero=prompt("ingrese");
+		numero=prompt("Ingrese el número "+contador);
 		acumulador=acumulador+parseInt(numero);
 
 	}
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=acumulador/contador;
 //hacer ferrete iluminacion. ejercicio 12. ejercicio 5. ejrecico 7. ejercicio 9. ejercicio 11.7.8.10.13
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,83 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+	var edad;
+	var sexo;
+	var estado;
+	edad=prompt("ingrese su edad");
+	while(edad<18 || edad>90)
+	{
+		alert("Reingrese su edad");
+		edad=prompt("ingrese su edad");
+
+	}
+	document.getElementById('Edad').value=edad+" Años";
+ 	
+ 	 	
+ 	sexo=prompt("Ingrese su sexo","M o F");
+ 	while(sexo!="M" && sexo!="F")
+ 	{
+ 		alert("Reingrese su SEXO");
+ 		sexo=prompt("Ingrese su SEXO","M o F");
+ 	}
+ 	switch(sexo)
+ 	{
+ 		case "M":document.getElementById('Sexo').value="Masculino";
+ 		break;
+ 		default:document.getElementById('Sexo').value="Femenino";
+ 	}
+
+ 	estado=prompt("Estado Civil. Ingrese: 1 Soltero. 2 Casado. 3 Divorciado. 4 Viudo","1, 2, 3, o 4");
+ 	while(estado<1 || estado>4)
+ 	{
+ 		alert("Estado Civil. Ingrese: 1 Soltero. 2 Casado. 3 Divorciado. 4 Viudo","1, 2, 3, o 4");
+ 		estado=prompt("Estado Civil. Ingrese: 1 Soltero. 2 Casado. 3 Divorciado. 4 Viudo","1, 2, 3, o 4");
+ 	}
+ 	switch(estado)
+ 	{
+ 		case "1":document.getElementById('EstadoCivil').value="Soltero";
+ 		break;
+ 		case "2":document.getElementById('EstadoCivil').value="Casado";
+ 		break;
+ 		case "3":document.getElementById('EstadoCivil').value="Divorciado";		
+ 		break;
+ 		default:document.getElementById('EstadoCivil').value="Viudo";
+ 		
+  	}
+  	var sueldo;
+  	sueldo=prompt("Ingrese su sueldo bruto");
+  	while(sueldo<=8000)
+  	{
+  		alert("ingrese su SUELDO");
+  		sueldo=prompt("Ingrese su sueldo bruto");
+  	}
+  	document.getElementById('Sueldo').value="$"+sueldo;
+  	
+  	var legajo;
+  	legajo=prompt("Ingrese Nº de legajo.");
+  	while(legajo<1000 || legajo>9999)
+  	{
+  		alert("Ingrese su Nº de legajo, SIN CEROS A LA IZQUIERDA y solo CUATRO CIFRAS");
+  		legajo=prompt("Ingrese Nº de legajo.");
+  	}
+  	document.getElementById('Legajo').value="Leg. Nº "+legajo;
+
+  	var nacionalidad;
+  	nacionalidad=prompt("Ingrese su nacionalidad","Ingrese A para Argentinos, E para Extranjeros, N para Nacionalizados");
+
+  	while(nacionalidad!="A" && nacionalidad!="E" && nacionalidad!="N")
+  	{
+  		alert("Ingrese correctamente su Nacionalidad");
+  		nacionalidad=prompt("Ingrese A para Argentinos, E para Extranjeros, N para Nacionalizados");
+
+  	}
+  	switch(nacionalidad)
+  	{
+  		case "A":document.getElementById('Nacionalidad').value="Argentino";
+  		break;
+  		case "E":document.getElementById('Nacionalidad').value="Extranjero";
+  		break;
+  		default:document.getElementById('Nacionalidad').value="Nacionalizado";
+  	}
+
 }
