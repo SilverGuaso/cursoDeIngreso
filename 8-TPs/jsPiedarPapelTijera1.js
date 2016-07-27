f53d63f837bd81b3569c3b02b1b08e6469af4968
silverguaso <silverguaso@hotmail.com>
2016-07-27 16:53:53 -0300

    Ejercicio PPT 1

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,68 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
-var eleccionMaquina;
 
+var eleccionMaquina;
 function comenzar()
 {
 	
-
+	eleccionMaquina=Math.floor(Math.random()*3+1);
+	
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
-
+	if (eleccionMaquina==1)
+	 {
+	 	alert("Usted empató");
+	 }
+	 else
+	 {
+	 	if (eleccionMaquina==2)
+	 	 {
+	 	 	alert("Usted perdió");
+	 	 }
+	 	 else
+	 	 {
+	 	 	alert("Usted ganó");
+	 	 }
+	 }	
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+	if (eleccionMaquina==1)
+	 {
+	 	alert("Usted ganó");
+	 }
+	 else
+	 {
+	 	if (eleccionMaquina==2)
+	 	 {
+	 	 	alert("Usted empató");
+	 	 }
+	 	 else
+	 	 {
+	 	 	alert("Usted perdió");
+	 	 }
+	 }
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	if (eleccionMaquina==1)
+	 {
+	 	alert("Usted perdió");
+	 }
+	 else
+	 {
+	 	if (eleccionMaquina==2)
+	 	 {
+	 	 	alert("Usted ganó");
+	 	 }
+	 	 else
+	 	 {
+	 	 	alert("Usted empató");
+	 	 }
+	 }
 
 }//FIN DE LA FUNCIÓN
